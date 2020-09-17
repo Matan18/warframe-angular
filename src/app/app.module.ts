2887682bd7e2ce43df73a2b5fc9f8509342c29e7
Mateus Andriola <mateusandriola18@gmail.com>
2020-09-17 11:42:55 -0300

    Feature: added invasions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { DarvoDetailComponent } from './darvo-detail/darvo-detail.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PlatformComponent } from './platform/platform.component';
+import { InvasionComponent } from './invasion/invasion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DarvoDetailComponent,
-    PlatformComponent
+    PlatformComponent,
+    InvasionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/invasion/invasion.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IInvasions } from './Invasion';
+import { ListService } from './list.service';
+
+@Component({
+  selector: 'app-invasion',
+  templateUrl: './invasion.component.html',
+  styleUrls: ['./invasion.component.sass']
+})
+export class InvasionComponent implements OnInit {
+
+  @Input() baseUrl: string;
+  constructor(
+    private listService: ListService
+  ) { }
+
+  invasions: Observable<IInvasions[]>;
+
+  ngOnInit(): void {
+    this.invasions = this.listService.getInvasions(this.baseUrl);
+  }
+
+}
