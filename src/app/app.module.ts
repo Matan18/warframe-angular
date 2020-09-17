9b7e7da7a3c984b28aff16a95fe79b8f0792572f
Mateus Andriola <mateusandriola18@gmail.com>
2020-09-17 16:02:05 -0300

    Feature: added Global updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { DarvoDetailComponent } from './darvo-detail/darvo-detail.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PlatformComponent } from './platform/platform.component';
+import { EarthcycleComponent } from './earthcycle/earthcycle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DarvoDetailComponent,
-    PlatformComponent
+    PlatformComponent,
+    EarthcycleComponent
   ],
   imports: [
     BrowserModule,
