27682795796783c2a0fbc980a46bcd0dc42b21db
Mateus Andriola <mateusandriola18@gmail.com>
2020-09-17 16:15:00 -0300

    Feature: construction Progress added

---
+++ src/app/construction-progress/construction-progress.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConstructionProgressComponent } from './construction-progress.component';
+
+describe('ConstructionProgressComponent', () => {
+  let component: ConstructionProgressComponent;
+  let fixture: ComponentFixture<ConstructionProgressComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ConstructionProgressComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConstructionProgressComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
