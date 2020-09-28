99702c595b092a6838391495f5fff151e10125a7
Mateus Andriola <mateusandriola18@gmail.com>
2020-09-28 16:08:34 -0300

    Syndicate with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { DarvoDetailComponent } from './darvo-detail/darvo-detail.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PlatformComponent } from './platform/platform.component';
+import { SyndicateComponent } from './notify-menu/syndicate/syndicate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DarvoDetailComponent,
-    PlatformComponent
+    PlatformComponent,
+    SyndicateComponent
   ],
   imports: [
     BrowserModule,
