de95cf7eef5c16a1ba2ebfd9125426af2b70eae7
Mateus Andriola <mateusandriola18@gmail.com>
2020-10-03 17:31:50 -0300

    Arsenal Initial styles

--- src/app/arsenal/arsenal.component.ts
+++ src/app/arsenal/arsenal.component.ts
@@ -10,7 +10,54 @@ export class ArsenalComponent implements OnInit {
   @Input() baseUrl: string;
   constructor() { }
 
+  menuItems = MenuItems;
+  selectedItem: string;
+
   ngOnInit(): void {
+    this.menuItems.map(item=>{
+      if(item.selected){
+        this.selectedItem=item.type
+      }
+    })
+  }
+
+  onItemClick(id: number) {
+    this.menuItems.map(item => {
+      item.selected = false
+      if (item.id == id) {
+        this.selectedItem = item.type
+        item.selected = true
+      }
+    })
+
   }
 
 }
+
+interface IMenuItem {
+  id: number;
+  name: string;
+  type: string;
+  selected: boolean;
+}
+
+const MenuItems: IMenuItem[] = [
+  {
+    id: 0,
+    name: 'Principal',
+    type: 'main',
+    selected: true
+  },
+  {
+    id: 1,
+    name: 'robot',
+    type: 'companion',
+    selected: false,
+  },
+  {
+    id: 2,
+    name: 'archwing',
+    type: 'archwing',
+    selected: false
+  }
+]
